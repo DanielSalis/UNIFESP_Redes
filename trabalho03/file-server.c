f590d3c109e246a69571eaf8eb057f09a029f1ff
Milena <milematoss@hotmail.com>
2021-02-16 19:52:50 -0300

    refatora os métodos para utilizar apenas as system calls 'open' e 'write'

--- trabalho03/file-server.c
+++ trabalho03/file-server.c
@@ -22,17 +22,18 @@
 #define COD_OK0_GET "OK-0 method GET OK\n"
 #define COD_OK1_FILE "OK-1 File open OK\n"
 #define COD_OK2_CREATE "OK-1 method CREATE OK\n"
-#define COD_OK3_APPEND "OK-2 method CREATE OK\n"
-#define COD_OK4_APPEND "OK-3 method CREATE OK\n"
+#define COD_OK3_APPEND "OK-2 method APPEND OK\n"
+#define COD_OK4_REMOVE "OK-3 method REMOVE OK\n"
 
 #define COD_ERROR_0_METHOD "ERROR-0 Method not supported\n"
-#define COD_ERROR_1_FILE "ERROR-1 File could not be open\n"
+#define COD_ERROR_1_FILE "ERROR-1 File could not be opened\n"
 #define COD_ERROR_2_FILE "ERROR-2 File already exists\n"
-#define COD_ERROR_3_FILE "ERROR-3 File not found\n"
+#define COD_ERROR_3_FILE "ERROR-3 File not found or could not be opened\n"
 #define COD_ERROR_4_FILE "ERROR-4 Can't append an empty text\n"
+#define COD_ERROR_5_FILE "ERROR-5 File not found or could not be removed\n"
 
-
-struct req_t{
+struct req_t
+{
     char method[128];
     char filename[256];
     char text[256];
@@ -40,7 +41,8 @@ struct req_t{
 
 typedef struct req_t req_t;
 
-struct conn_params{
+struct conn_params
+{
     pthread_t tid;
     int sc;
     struct sockaddr_in caddr;
@@ -48,38 +50,44 @@ struct conn_params{
 
 typedef struct conn_params conn_params;
 
-void get_request(req_t *r, char *rstr){
+void get_request(req_t *r, char *rstr)
+{
     bzero(r, sizeof(req_t));
     sscanf(rstr, "%s %s %s", r->method, r->filename, r->text);
 }
 
-void send_file(int sockfd, req_t r){
+void send_file(int sockfd, req_t r)
+{
     int fd, nr;
     unsigned char fbuff[FBLOCK_SIZE];
     fd = open(r.filename, O_RDONLY, S_IRUSR);
-    if(fd == -1){
+    if (fd == -1)
+    {
         perror("open");
         send(sockfd, COD_ERROR_1_FILE, strlen(COD_ERROR_1_FILE), 0);
         return;
     }
     send(sockfd, COD_OK1_FILE, strlen(COD_OK1_FILE), 0);
-    do{
+    do
+    {
         bzero(fbuff, FBLOCK_SIZE);
         nr = read(fd, fbuff, FBLOCK_SIZE);
-        if(nr > 0){
+        if (nr > 0)
+        {
             send(sockfd, fbuff, nr, 0);
         }
-    }while(nr > 0);
+    } while (nr > 0);
 
     close(fd);
     return;
 }
 
-void create_file(int sockfd, req_t r){
-    int fd = open((char *)(r.filename), O_RDONLY, S_IRUSR);
-    if(fd == -1){
-        FILE *fp  = fopen ((char *)(r.filename), "w");
-        fclose (fp);
+void create_file(int sockfd, req_t r)
+{
+    int fd = open(r.filename, O_RDONLY, S_IRUSR);
+    if (fd == -1)
+    {
+        fd = open(r.filename, O_WRONLY | O_CREAT, S_IRUSR);
         close(fd);
         return;
     }
@@ -89,63 +97,72 @@ void create_file(int sockfd, req_t r){
     return;
 }
 
-void append_to_file(int sockfd, req_t r){
-    if(strlen(r.text) == 0){
-        perror("append");
+void append_to_file(int sockfd, req_t r)
+{
+    if (strlen(r.text) == 0)
+    {
         send(sockfd, COD_ERROR_4_FILE, strlen(COD_ERROR_4_FILE), 0);
         return;
     }
 
-    int fd = open((char *)(r.filename), O_APPEND, S_IRUSR);
-    if(fd == -1){
+    int fd = open(r.filename, O_WRONLY | O_APPEND, S_IRUSR);
+    if (fd == -1)
+    {
         perror("append");
         send(sockfd, COD_ERROR_3_FILE, strlen(COD_ERROR_3_FILE), 0);
+        close(fd);
         return;
     }
 
-    FILE *fa  = fopen ((char *)(r.filename), "a");
-    fprintf(fa, "\n %s \n", r.text);
-    
-    fclose (fa);
+    write(fd, &r.text, strlen(r.text));
     close(fd);
     return;
 }
 
-void remove_file(int sockfd, req_t r){
-    int fd = open((char *)(r.filename), O_RDONLY, S_IRUSR);
-    if(fd == -1){
-        perror("delete");
-        send(sockfd, COD_ERROR_3_FILE, strlen(COD_ERROR_3_FILE), 0);
+void remove_file(int sockfd, req_t r)
+{
+    int fd = open(r.filename, O_WRONLY | O_TRUNC, S_IRUSR);
+    if (fd == -1)
+    {
+        perror("remove");
+        send(sockfd, COD_ERROR_5_FILE, strlen(COD_ERROR_5_FILE), 0);
+        close(fd);
         return;
     }
-    remove(r.filename);
     close(fd);
     return;
 }
 
-void proc_request(int sockfd, req_t r){
-    if (strcmp(r.method, REQ_GET) == 0){
-        send(sockfd, COD_OK0_GET, strlen(COD_OK0_GET),0);
+void proc_request(int sockfd, req_t r)
+{
+    if (strcmp(r.method, REQ_GET) == 0)
+    {
+        send(sockfd, COD_OK0_GET, strlen(COD_OK0_GET), 0);
         send_file(sockfd, r);
     }
-    else if(strcmp(r.method, REQ_CREATE) == 0){
-        send(sockfd, COD_OK2_CREATE, strlen(COD_OK2_CREATE),0);
+    else if (strcmp(r.method, REQ_CREATE) == 0)
+    {
+        send(sockfd, COD_OK2_CREATE, strlen(COD_OK2_CREATE), 0);
         create_file(sockfd, r);
     }
-    else if(strcmp(r.method, REQ_APPEND) == 0){
-        send(sockfd, COD_OK3_APPEND, strlen(COD_OK3_APPEND),0);
+    else if (strcmp(r.method, REQ_APPEND) == 0)
+    {
+        send(sockfd, COD_OK3_APPEND, strlen(COD_OK3_APPEND), 0);
         append_to_file(sockfd, r);
     }
-    else if(strcmp(r.method, REQ_REMOVE) == 0){
-        send(sockfd, COD_OK3_APPEND, strlen(COD_OK3_APPEND),0);
+    else if (strcmp(r.method, REQ_REMOVE) == 0)
+    {
+        send(sockfd, COD_OK4_REMOVE, strlen(COD_OK4_REMOVE), 0);
         remove_file(sockfd, r);
     }
-    else{
-        send(sockfd, COD_ERROR_0_METHOD, strlen(COD_ERROR_0_METHOD),0);
+    else
+    {
+        send(sockfd, COD_ERROR_0_METHOD, strlen(COD_ERROR_0_METHOD), 0);
     }
 }
 
-void *connect_client(void *args){
+void *connect_client(void *args)
+{
     conn_params conn = *(conn_params *)args;
     int nr;
     char request[MAX_REQ_LEN];
@@ -153,7 +170,8 @@ void *connect_client(void *args){
     printf("Conectado com cliente %s:%d\n", inet_ntoa(conn.caddr.sin_addr), ntohs(conn.caddr.sin_port));
     bzero(request, MAX_REQ_LEN);
     nr = recv(conn.sc, request, MAX_REQ_LEN, 0);
-    if(nr > 0){
+    if (nr > 0)
+    {
         get_request(&req, request);
         printf(" method: %s\n filename: %s\n", req.method, req.filename);
         proc_request(conn.sc, req);
@@ -162,14 +180,17 @@ void *connect_client(void *args){
     pthread_exit(NULL);
 }
 
-int main(int argc, char **argv){
-    if(argc != 2){
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
         printf("Uso %s <porta>n\n", argv[0]);
         return 0;
     }
     int sl;
     sl = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if(sl == -1){
+    if (sl == -1)
+    {
         perror("socket");
         return 0;
     }
@@ -177,36 +198,40 @@ int main(int argc, char **argv){
     saddr.sin_port = htons(atoi(argv[1]));
     saddr.sin_family = AF_INET;
     saddr.sin_addr.s_addr = INADDR_ANY;
-    if(bind(sl,(struct sockaddr *)&saddr,
-        sizeof(struct sockaddr_in)) == -1){
-            perror("bind");
-            return 0;
+    if (bind(sl, (struct sockaddr *)&saddr,
+             sizeof(struct sockaddr_in)) == -1)
+    {
+        perror("bind");
+        return 0;
     }
-    if(listen(sl,1000) == -1){
+    if (listen(sl, 1000) == -1)
+    {
         perror("listen");
         return 0;
     }
 
     struct sockaddr_in caddr;
     int addr_len;
-    int sc;   
+    int sc;
     struct conn_params conn[MAX_REQ_LEN];
     while (1)
     {
         addr_len = sizeof(struct sockaddr_in);
-        sc = accept(sl, (struct sockaddr*)&caddr,(socklen_t *)&addr_len);
-        if(sc == -1){
+        sc = accept(sl, (struct sockaddr *)&caddr, (socklen_t *)&addr_len);
+        if (sc == -1)
+        {
             perror("accept");
             continue;
         }
-        conn[sc].sc = sc; 
+        conn[sc].sc = sc;
         conn[sc].caddr = caddr;
         pthread_create(&conn[sc].tid, NULL, connect_client, &conn[sc]);
     }
-    int i=0;
-	while (i < MAX_REQ_LEN){
-		pthread_join(conn[i].tid, NULL);
-	}
+    int i = 0;
+    while (i < MAX_REQ_LEN)
+    {
+        pthread_join(conn[i].tid, NULL);
+    }
     close(sl);
     return 0;
 }
